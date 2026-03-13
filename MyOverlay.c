921407525bf8002f27c2d216a33c6a7716cc9fe3
Marek <marek.sed2002@gmail.com>
2026-03-13 12:47:24 +0000

    add mount, unmount to virtual filesystem

--- MyOverlay.c
+++ MyOverlay.c
@@ -2,41 +2,75 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/pagemap.h> // for simple_dir_operations/simple_inode_operations
 
+
+// macros used for metadata
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Marek");
 MODULE_DESCRIPTION("A Simple Layered Filesystem (Overlay)");
 
-// This function is called when the Kernel tries to mount our FS
-static struct dentry *mount(struct file_system_type *fs_type, 
-                               int flags, const char *dev_name, void *data) {
-    // For now, we'll just print a message. 
-    // In a real driver, this is where we'd link the lower and upper folders.
-    printk(KERN_INFO "Overlay: Mount attempted on device %s\n", dev_name);
-    return mount_nodev(fs_type, flags, data, NULL); 
-}
 
-// Defining our Filesystem structure
-static struct file_system_type fs_type = {
-    .owner = THIS_MODULE,
-    .name = "overlay",      // This is the name used in the 'mount -t' command
-    .mount = mount,
-    .kill_sb = kill_anon_super, // Standard way to 'unmount' a virtual FS
+// Here starts my journey :)
+
+static const struct super_operations my_super_ops = {
+    .statfs = simple_statfs,
+    .drop_inode = generic_delete_inode,
 };
 
-// When the module is loaded (insmod)
-static int __init my_overlay_init(void) {
-    int ret = register_filesystem(&fs_type);
-    if (ret == 0)
-        printk(KERN_INFO "Overlay: Filesystem registered successfully!\n");
-    return ret;
+static int my_fill_super(struct super_block *sb, void *data, int silent) { 
+    struct inode *inode = new_inode(sb);
+
+    inode->i_ino = 1; // Set inode number (1 is standard for root)
+    inode->i_mode =  S_IFDIR | 0755; // Set as directory with permissions
+    inode->i_op = &simple_dir_inode_operations; // Assign directory operations
+    inode->i_fop = &simple_dir_operations;// Assign file operations
+
+    sb->s_op = &my_super_ops; // Required: superblock operations
+    sb->s_root = d_make_root(inode); // Required: root dentry
+    sb->s_maxbytes = MAX_LFS_FILESIZE; // maximum size allowed by the kernel and architecture
+    return 0; 
 }
 
-// When the module is removed (rmmod)
-static void __exit my_overlay_exit(void) {
-    unregister_filesystem(&fs_type);
-    printk(KERN_INFO "Overlay: Filesystem unregistered.\n");
+/* mount_nodev is kernel helper function for mounting filesystems that do not use a physical device
+it creates a root dentry (the root director entry for my filesystem)
+it calls a function to set up the superblock and root inode (NULL is passed no custom setup yet)
+In are case it's useful because are goals is not to manage the storage device, instead we want to merge existing directories from other filesystems.
+*/
+static struct dentry *my_mount_function(struct file_system_type *fs_type, int flags, const char *dev_name, void*data) {
+    printk(KERN_INFO "mounted function called \n"); // kernel log level
+    return mount_nodev(fs_type, flags, data, my_fill_super); // GOOD: kernel calls function safely
 }
 
-module_init(my_overlay_init);
-module_exit(my_overlay_exit);
+static void my_kill_sb(struct super_block *sb) {
+    printk(KERN_INFO "OverlayFS: unmounted\n");
+    kill_anon_super(sb);
+}
+
+/* I am not defining a new struct type but creating a variable of an existing struct type and initializing its field. 
+kernel's built-in struct called struct file_system_type
+static -> so not accessible out of the module 
+*/
+static struct file_system_type myfs_type ={
+    .name = "overlayfs",
+    .owner = THIS_MODULE,
+    .mount = my_mount_function,                    
+    .kill_sb = my_kill_sb, // the method to call when an instance of this filesystem should be shut down
+};
+
+/*creating init and exit function, the kernel calls these when module is loaded or unloaded*/
+static int __init myfs_init(void){
+    printk(KERN_INFO "OverlayFS: registered\n");
+    return register_filesystem(&myfs_type);
+}
+static void __exit myfs_exit(void){
+    unregister_filesystem(&myfs_type);
+    printk(KERN_INFO "OverlayFS: unregistered\n");
+}
+
+// these are marcros that tell kernel which functions to call when module is loaded and unloaded
+module_init(myfs_init); // insmod   
+module_exit(myfs_exit); // rmmod
+
+
+//Add minimal virtual filesystem module with root inode
